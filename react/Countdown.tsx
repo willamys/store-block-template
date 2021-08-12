c5638fafa4c0b5a893f6ee8eb1eb6d643c184539
Willamys Araújo <willamysaraujo7@gmail.com>
2021-08-11 21:46:49 -0300

    countdown first

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,16 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TimeSplit } from './typings/global';
+import { tick, getTwoDaysFromNow } from './utils/time';
+import { useCssHandles } from 'vtex.css-handles';
+import { FormattedMessage } from 'react-intl';
 
-interface CountdownProps {}
+const CSS_HANDLES = ['container', 'countdown', 'title',];
+const DEFAULT_TARGET_DATE = getTwoDaysFromNow();
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({}) => {
-  return <div />
+interface CountdownProps {
+  title: string;
+  targetDate: string;
 }
 
-Countdown.schema = {
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
+  title,
+  targetDate = DEFAULT_TARGET_DATE, }) => {
+
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: '00',
+    minutes: '00',
+    seconds: '00',
+  });
+
+  const handles = useCssHandles(CSS_HANDLES);
+  const titleText = title || <FormattedMessage id="countdown.title" />;
+  tick(targetDate, setTime);
+  //<h1>{targetDate}</h1>
+  /**
+   * *tachyons
+   * <div className={`${handles.countdown} c-muted-1 db tc`}>
+   * c-muted-1 =>cor do texto
+   * db=> display-block 
+   * tc=>text center
+   */
+  return (
+    <div className={`${handles.container} t-heading-2 fw3 w-100 c-muted-1`}>
+      <div className={`${handles.title} db tc`}>{titleText}</div>
+      <div className={`${handles.countdown} db tc`}>
+        {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
+      </div>
+    </div>
+  );
+}
+
+Countdown.schema = { //informações que serão lidas no editor do site
   title: 'editor.countdown.title',
   description: 'editor.countdown.description',
   type: 'object',
-  properties: {},
+  properties: {
+    title: {
+      title: 'Sou um título',
+      type: 'string',
+      default: null,
+    },
+    targetDate: {
+      title: 'Data final',
+      description: 'Data final utilizada no contador',
+      type: 'string',
+      default: null,
+    },
+  },
 }
 
 export default Countdown
